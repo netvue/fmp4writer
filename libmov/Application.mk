1c079a49ef2c8f0e945970e9592f147e3bf2d498
Docee <docee@163.com>
2022-05-18 20:25:23 +0800

    add Android X86 target

--- libmov/Application.mk
+++ libmov/Application.mk
@@ -1,4 +1,4 @@
 APP_BUILD_SCRIPT := ./Android.mk
 APP_STL := c++_static
-APP_ABI := armeabi-v7a arm64-v8a x86_64
+APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
 #APP_PLATFORM := android-21 #SLES SLAndroidDataFormat_PCM_EX
